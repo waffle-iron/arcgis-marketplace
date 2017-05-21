62fd9267a209e8f514fbd58cc26611cd5cdec589
mongkok <dani.pyc+github@gmail.com>
2017-05-21 15:51:10 +0200

    up!

--- tests/api/test_exceptions.py
+++ tests/api/test_exceptions.py
@@ -18,5 +18,4 @@ class ExceptionTests(BaseViewTests):
         )
 
         response = self.client.get(self.reverse('me-list'))
-
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
